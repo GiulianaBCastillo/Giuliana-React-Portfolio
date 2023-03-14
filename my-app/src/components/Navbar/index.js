9d6547c013e4a090d40110172f66a8983a2a17fe
GiulianaBCastillo <giuliana.b.castillo@gmail.com>
2023-03-14 11:45:02 +0000

    navbar, about, contact, home and works folders added to app.js

---
+++ my-app/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+import {useState} from 'react';
+
+const navBar = () => {
+    <nav>
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+    </nav>
+}
+
+export default navBar;
